f0183168ffc3525bb3c04ba40619931a8444e2b6
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-25 15:28:30 +0600

    fetch and load data done

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Books = () => {
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+        fetch('books.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    } , []);
     return (
         <div>
-            <h1 className='text-xl py-4 font-semibold'>Select Today's Reading...</h1>
+            <h1 className='text-xl py-4 font-semibold px-2'>Select Today's Reading...</h1>
+            <div>
+
+            </div>
         </div>
     );
 };
