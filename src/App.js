c2961611b05ac4f91458c55c147b55b367d11c15
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-25 15:21:57 +0600

    books component create done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
 import Cart from './components/Cart/Cart';
+import Navbar from './components/Navbar/Navbar';
+import Books from './components/Books/Books';
 
 const style = {
   backgroundColor: 'rgb(242, 244, 250)',
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className='grid grid-cols-4 gap-4'>
       <div style={style} className='col-span-3 py-24 px-32'>
-        <Header></Header>
+        <Navbar></Navbar>
+        <Books></Books>
       </div>
       <div>
         <Cart></Cart>
---
+++ src/components/Books/Books.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Books = () => {
+    return (
+        <div>
+            <h1>All the books here...</h1>
+        </div>
+    );
+};
+
+export default Books;
