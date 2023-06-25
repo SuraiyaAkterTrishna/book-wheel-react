975b358ca40da82ea7cb6ea6a81b20c66cd1ad88
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-25 20:22:41 +0600

    add to list button clicked

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -2,6 +2,9 @@ import React from "react";
 
 const Book = ({ book }) => {
   const { id, image, age, time, name, detail } = book;
+  const handleAddToList = (id) => {
+    console.log('button clicked for', id);
+  }
   return (
     <div className="card card-compact bg-base-100 shadow-xl mx-auto">
       <figure>
@@ -16,7 +19,7 @@ const Book = ({ book }) => {
         <p>For Age: {age}</p>
         <p>Time Required: {time}s</p>
         <div className="card-actions">
-          <button className="btn btn-primary w-full">Add to List</button>
+          <button onClick={() => handleAddToList(id)} className="btn btn-primary w-full">Add to List</button>
         </div>
       </div>
     </div>
