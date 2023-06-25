ba1f45ae5ac4b976271a5f22a78d757dc73d9a7a
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-25 12:51:47 +0600

    install daisy ui and divide two part done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Cart from './components/Cart/Cart';
 
+const style = {
+  backgroundColor: 'rgb(242, 244, 250)',
+}
 function App() {
   return (
-    <div>
-      <h1>Start Your Reading Here..</h1>
+    <div className='grid grid-cols-4 gap-4'>
+      <div style={style} className='col-span-3'>
+        <Header></Header>
+      </div>
+      <div>
+        <Cart></Cart>
+      </div>
     </div>
   );
 }
